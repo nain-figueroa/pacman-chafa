40aa2df782aa15f016b0c9d3e542e14cd1d790db
Nain Figueroa <nainzavala7@gmail.com>
2026-07-27 18:04:17 -0700

    Modificación de la configuración del proyecto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
